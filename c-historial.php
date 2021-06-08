cc5ecb9053b14f5c11743365981834e4b5f0ac04
Javerson287 <81368864+Javerson287@users.noreply.github.com>
2021-06-08 10:52:50 -0500

    Add files via upload

---
+++ c-historial.php
@@ -0,0 +1,5 @@
+<?php
+
+$menu = "v-historial.php";
+
+include("plantilla-2.php");
---
+++ v-historial.php
@@ -0,0 +1,62 @@
+<html>
+
+<head>
+
+
+<title></title>
+
+
+
+</head>
+
+<body>
+
+
+      
+
+
+            
+        <div id = centrar> 
+
+                  <h1>TypKey</h1></div>
+
+                  <hr style="height:2px;border-width:0;background-color:blue">
+            
+
+            <br> 
+
+
+                  
+            <div class="field" id="buscar">
+
+            
+      <input type="text" id="buscar" placeholder="Buscar Registro"/>
+
+            </div>
+
+            <div class = "bs">
+            <button type="button" id="search" >Buscar</button>     
+            </div>
+
+
+            <div class = "ambiente">
+            <h1>Ambiente</h1>   
+            </div>
+
+
+
+            <div class = "fecha">
+            <h1>Fecha</h1>   
+            </div>
+
+            <div class = "Descargar">
+            <h1>Descargar</h1>   
+            </div>
+
+
+
+
+
+</body>
+</html>
+
